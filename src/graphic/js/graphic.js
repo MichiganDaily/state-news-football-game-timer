f6086638e06c3b4a90906f27f1807f5b7de5cf28
erxclau <ericlau41802@gmail.com>
2021-10-24 23:07:14 -0400

    Create timer

--- src/graphic/js/graphic.js
+++ src/graphic/js/graphic.js
@@ -1,20 +1,29 @@
 // JS for your graphic
 import pym from "pym.js";
-import * as d3 from "d3";
+import { interval, selectAll, select, format } from "d3";
+import { DateTime } from "luxon";
 
 const draw = () => {
-  // step 1: access data
-  // step 2: create chart dimensions
-  // step 3: draw canvas
-  // step 4: create scales
-  // step 5: draw data
-  // step 6: draw peripherals
-  // step 7: set up interactions
-}
+  const last = DateTime.fromISO("2005-09-30T20:30");
+  const intervals = ["years", "months", "days", "hours", "minutes", "seconds"];
+  interval(() => {
+    const now = DateTime.fromJSDate(new Date());
+    const d = now.diff(last, [...intervals, "milliseconds"]).toObject();
 
-window.onresize = () => { };
+    d.seconds = format(".2f")(d.seconds + (Math.floor(d.milliseconds / 10) / 100));
+
+    selectAll(".interval")
+      .each(function() {
+        if (d[this.id] === 0 && this.id === "months") {
+          select(this).style("display", "none");
+        }
+        select(this).select(".number").text(d[this.id])
+      });
+  }, 25);
+}
 
 window.onload = () => {
   const pymChild = new pym.Child({ polling: 500 });
   pymChild.sendHeight();
+  draw();
 };
